fda7d74d6b18a0ac7599d9166ccded2280ed4874
Moises Gualapuro <43993933+gualapuromoises@users.noreply.github.com>
2023-06-16 00:07:22 +0000

    modulo largogc creado

---
+++ largogc.py
@@ -0,0 +1,45 @@
+def largo(seq):
+    """
+    largo es una función que calcula el número de carcateres de una secuencia
+    input: seq es una secuencia de caracteres
+    output: len_seq es un valor numérico de la cantidad de caracteres
+    """
+    len_seq = len(seq)
+    return(len_seq)
+
+def gc(seq):
+    """
+    gc es una función que calcula el procentaje de nucleótidos g y c
+    input: seq secuencia de nucleótidos
+    vars: 
+        c cantidad de citosinas, 
+        g cantidad de guaninas, 
+        n longitud de la secuencia 
+    output: gcp porcentaje de g y c
+    """
+    c = seq.count("C")
+    g = seq.count("G")
+    n = len(seq)
+    gc = round(100*(c+g)/n, 3)
+    return gc
+
+def restriccion(seq, enzima):
+    """
+    restriccion es una función de digestión de secuencias utilizando enzimas
+    input: 
+        seq es una secuencia de nucléotidos
+        enzima es un fragmento de nucleótidos que indica donde se corta la secuencia
+    var: 
+
+    output: 
+        fragmentos: cada uno de los fragmentos digeridos/cortados
+        longitud: largo de cada fragmento
+    """
+    frags = []
+    ni = seq.find(enzima)
+    frag1 = seq[:ni+len(enzima)]
+    frag2 = seq[ni+len(enzima):]
+    frags.append([frag1, len(frag1)])
+    frags.append([frag2, len(frag2)])
+
+    return (frags)
