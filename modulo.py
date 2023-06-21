3381a99dc7320f0413b2352c487c39c861a62711
Moises Gualapuro <43993933+gualapuromoises@users.noreply.github.com>
2023-06-21 19:18:09 +0000

    Cambios en modulo.py

---
+++ modulo.py
@@ -0,0 +1,8 @@
+def finder(seq):
+    """
+    largo es una función que calcula el número de carcateres de una secuencia
+    input: seq es una secuencia de caracteres
+    output: len_seq es un valor numérico de la cantidad de caracteres
+    """
+    frag = seq.find("ATC")
+    return(frag)
